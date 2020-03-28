e8287db8363929cb65ab0c602edb6d7aa7826bf8
Worldfrom6feet <dhruvilmaniar@gmail.com>
2020-03-28 19:42:04 +0530

    Added Image in Feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
+import FeedImage from './pokeball.png'
 
 export default class Feed extends Component {
 
@@ -23,20 +24,32 @@ export default class Feed extends Component {
 
         const {posts} = this.state
 
+        const ImageStyle = {
+            "position": "absolute",
+            "top": "20px",
+            "left": "-100px",
+            "opacity": '0.6',
+        }
+
+        const postStyle = {
+            "overflow" : "hidden",
+            "padding-left" : "80px"
+        }
+
         const postsList = posts.length ? (
             posts.map((post) => {
                 return (
-                    <div className="post card" key={post.id}>
+                    <div className="post card" style={postStyle} key={post.id}>
+                        <img style={ImageStyle} src={FeedImage} alt="Feed" />
                         <div className="card-content">
                             <Link to={"/Motivation/" + post.id}>
-                                <span className="card-title">{post.title}</span>
-                                <p>{post.body}</p>
+                                <span className="card-title blue-grey-text darken-4"><b>{post.title}</b></span>
                             </Link>
+                            <p>{post.body}</p>
                         </div>
                     </div>
                 )
-            }
-            )
+            })
         ) : (
             <div>No Posts Yet!! </div>
         )
